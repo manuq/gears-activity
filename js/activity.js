57bbf31581d16caa942168556c322c8215f1d9c1
Manuel Quiñones <manuq@laptop.org>
2013-08-07 16:17:35 -0300

    Add great gearsketch
    Use requirejs shim to do so.

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,16 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
+    require("gearsketch_util");
+    require("gearsketch_model");
+    require("gearsketch_main");
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
 
         // Initialize the activity.
         activity.setup();
+
+        new window.gearsketch.GearSketch();
 
     });
 
--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,21 @@
 requirejs.config({
     baseUrl: "lib",
+    shim: {
+        gearsketch_util: {
+            exports: 'gearsketch/gearsketch_util'
+        },
+        gearsketch_model: {
+            exports: 'gearsketch/gearsketch_model'
+        },
+        gearsketch_main: {
+            exports: 'gearsketch/gearsketch_main'
+        }
+    },
     paths: {
-        activity: "../js"
+        activity: "../js",
+        gearsketch_util: "../lib/gearsketch/gearsketch_util",
+        gearsketch_model: "../lib/gearsketch/gearsketch_model",
+        gearsketch_main: "../lib/gearsketch/gearsketch_main"
     }
 });
 
